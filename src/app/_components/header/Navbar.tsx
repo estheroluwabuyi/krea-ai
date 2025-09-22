6a6aa90a9eb537dec3f93512ff94ff45f0b3d940
Esther Okiki <estherokiki15@gmail.com>
2025-09-22 15:32:58 +0100

    change placeholder logo to krea logo

--- src/app/_components/header/Navbar.tsx
+++ src/app/_components/header/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
           className="w-[25px] h-[25px] "
         />
         <div className="flex justify-between items-center ">
-          <div className="w-[18px] h-[18px] rounded-full bg-[radial-gradient(circle_at_center,#2563eb,pink)]"></div>
+          <div className="w-[17px] h-[17px] rounded-full bg-[radial-gradient(circle_at_center,#2563eb,pink)]"></div>
 
           <button className="text-text/80 sm:flex justify-between items-center gap-3 hidden ">
             <span className="ml-3 text-[1.15rem] ">benevolentcodegal</span>
@@ -42,63 +42,63 @@ export default function Navbar() {
 
       {/* Second container */}
       <div className="hidden lg:flex  bg-gray/15 dark:bg-gray/30 gap-[2rem] py-3 pr-4 pl-1.5 rounded-xl justify-center items-center">
-        <button className="bg-background p-1.5 px-2 rounded-xl shadow-md backdrop-blur-md transition-all duration-500 ease-in-out">
-          <FaHouse size={18} />
+        <button className="bg-background px-3 py-2 rounded-xl shadow-md backdrop-blur-md transition-all duration-500 ease-in-out">
+          <FaHouse size={17} />
         </button>
 
         <button className="btn-icon">
-          <FaRegImage size={18} />
+          <FaRegImage size={17} />
         </button>
         <button className="btn-icon">
-          <FaVideo size={18} />
+          <FaVideo size={17} />
         </button>
-        <button className="btn-icon">
-          <MdAutoFixHigh size={18} />
+        <button className="btn-icon -rotate-90">
+          <MdAutoFixHigh size={17} />
         </button>
 
         <button className="btn-icon">
           <Image
             src={isDark ? "/images/pen-white.svg" : "/images/pen.svg"}
             alt="pen"
-            width={18}
-            height={18}
-            className="rotate-80  w-[18px] h-[18px] "
+            width={17}
+            height={17}
+            className="rotate-80  w-[17px] h-[17px] "
           />
         </button>
 
         <button className="btn-icon">
-          <FaPenToSquare size={18} />
+          <FaPenToSquare size={17} />
         </button>
 
         <button className="btn-icon">
-          <FaFolder size={18} />
+          <FaFolder size={17} />
         </button>
       </div>
 
       {/* Third Container */}
       <div className="flex justify-between items-center gap-[2rem]">
         <button className="flex  bg-gray/15 justify-between items-center gap-2 p-1.5 rounded-2xl dark:bg-gray/30 hover:bg-gray/40 transition-all duration-500 ease-in-out">
-          <IoMdPhotos size={18} />
+          <IoMdPhotos size={17} />
           <span className="hidden md:inline text-[1.25rem]">Gallery</span>
         </button>
 
         <button className="flex  bg-gray/15 justify-between items-center gap-2 p-1.5 rounded-2xl dark:bg-gray/30 hover:bg-gray/40 hover:bg-gray/40 transition-all duration-500 ease-in-out">
-          <BiSupport size={18} />
+          <BiSupport size={17} />
           <span className="hidden md:inline text-[1.25rem]">Support</span>
         </button>
 
-        <button className="p-1.5 bg-gray/15 dark:bg-gray/30 rounded-xl">
-          <FaBell size={18} />
+        <button className="p-2 bg-gray/15 dark:bg-gray/30 rounded-xl">
+          <FaBell size={17} />
         </button>
 
         <button
-          className="p-1.5 bg-gray/15 dark:bg-gray/30 rounded-xl"
+          className="p-2 bg-gray/15 dark:bg-gray/30 rounded-xl"
           onClick={() => setIsDark((prev) => !prev)}
         >
-          {isDark ? <FaMoon size={18} /> : <MdWbSunny size={18} />}
+          {isDark ? <FaMoon size={17} /> : <MdWbSunny size={17} />}
         </button>
 
-        <div className="w-[18px] h-[18px] rounded-full bg-[radial-gradient(circle_at_center,#2563eb,pink)]"></div>
+        <div className="w-[17px] h-[17px] rounded-full bg-[radial-gradient(circle_at_center,#2563eb,pink)]"></div>
       </div>
     </nav>
   );
